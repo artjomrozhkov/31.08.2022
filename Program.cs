9a9624115b756a05886e528ea1cd4acbef6f3d3b
opilane <opilane@HP-CZC6297BB7>
2022-09-07 15:43:43 +0300

    abc

--- Program.cs
+++ Program.cs
@@ -22,14 +22,33 @@ foreach (Isik x in people)
 {
     x.print_Info();
 
-    to_file.WriteLine(x.nimi + ", " + x.arvutaVanus() + ", " + x.isikSugu + ";");
+    to_file.WriteLine(x.nimi + ", " + x.arvutaVanus() + ", " + x.isikSugu + ", " + x.synniaasta + ", " + x.palk + ", "+ x.tulumaks + ", "+ x.maksuvaba + ", "+ x.sissetulek + ";" );
 }
 to_file.Close();
-List<Isik> LoetudFiles = new List<Isik>();
+List<Tootaja> tootajad = new List<Tootaja>();
 StreamReader sr = new StreamReader(@"..\..\..\spisok.txt");
-List<string> lines = new List<string>();
-int n = sr.ReadToEnd().Split(new char[] { '\n' }).Length;
-Console.WriteLine("-----" + n+"-----");
+string text;
+while ((text = sr.ReadLine()) != null)
+{
+    string[] rida = text.Split(',');
+    tootajad.Add(new Tootaja(rida[0], rida[1], int.Parse(rida[2]), rida[3], int.Parse(rida[4]), TextSugu(rida[5]), double.Parse(rida[6]), double.Parse(rida[7])));
+}
+sr.Close();
+foreach (var item in tootajad)
+{
+    Console.WriteLine(item.nimi + " " + item.isikSugu);
+}
+
+Isik.Sugu TextSugu(string andmed)
+{
+    switch (andmed)
+    {
+        case "emane":
+            return Isik.Sugu.emane;
+        default:
+            return Isik.Sugu.isane;
+    }
+}
 
 
 
